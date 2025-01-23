698c8151741897e96662214f9d0827788670d8ac
Carl Green <carl.green@gronare.com>
2025-01-23 22:26:59 +0100

    adds vim-test to nvim

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -1,16 +1,14 @@
 return {
   "vim-test/vim-test",
   dependencies = {
-    "preservim/vimux",
-  },
-  keys = {
-    { "<leader>t", ":TestNearest<CR>", desc = "Run Nearest Test" },
-    { "<leader>T", ":TestFile<CR>", desc = "Run Test File" },
-    { "<leader>a", ":TestSuite<CR>", desc = "Run Test Suite" },
-    { "<leader>l", ":TestLast<CR>", desc = "Run Last Test" },
-    { "<leader>g", ":TestVisit<CR>", desc = "Visit Last Test File" },
+    "preservim/vimux"
   },
   config = function()
-    vim.g["test#strategy"] = "vimux" -- Set the test execution strategy
+    vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", {})
+    vim.keymap.set("n", "<leader>T", ":TestFile<CR>", {})
+    vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", {})
+    vim.keymap.set("n", "<leader>l", ":TestLast<CR>", {})
+    vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", {})
+    vim.cmd("let test#strategy = 'vimux'")
   end,
 }
