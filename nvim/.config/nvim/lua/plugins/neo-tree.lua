4e030fac299e6a77baf57093fe43bb294a110e89
Carl Green <carl.green@gronare.com>
2025-01-23 18:32:02 +0100

    show hidden files in file tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,20 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal toggle left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal toggle float<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- Show hidden or filtered files
+      },
+    },
+  },
+  keys = {
+    -- Define key mappings
+    { "<C-n>", ":Neotree filesystem reveal toggle left<CR>", desc = "Toggle Neo-tree (filesystem)" },
+    { "<leader>bf", ":Neotree buffers reveal toggle float<CR>", desc = "Toggle Neo-tree (buffers)" },
+  },
 }
