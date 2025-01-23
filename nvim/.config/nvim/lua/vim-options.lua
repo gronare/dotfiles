9b3efdd6a1b5b5cfc106bf03d32c765e3aea38f4
Carl Green <green@MeanMachine.localdomain>
2025-01-23 15:04:12 +0100

    basic setup

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,13 @@
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.g.mapleader = " "
+
+vim.opt.swapfile = false
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
+
+vim.keymap.set("v", "<leader>c", '"+y', { noremap = true, silent = true })
+
