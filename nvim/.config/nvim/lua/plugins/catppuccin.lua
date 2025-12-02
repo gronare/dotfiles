94f929fbe96337839728060e196299c218ea03b6
Carl Green <carl.green@auctionet.com>
2025-12-02 01:26:02 +0100

    improvements

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -8,6 +8,17 @@ return {
     config = function()
       require("catppuccin").setup({
         transparent_background = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          telescope = true,
+          treesitter = true,
+          lsp_trouble = true,
+          lsp_saga = true,
+          nvimtree = true,
+          notify = true,
+          which_key = true,
+        },
       })
       vim.cmd.colorscheme "catppuccin-mocha"
     end
